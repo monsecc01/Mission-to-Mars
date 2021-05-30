c28e253967e4865c9c06c1c3e5eeafc967a4eb9c
monsecc01 <81447450+monsecc01@users.noreply.github.com>
2021-05-30 11:28:18 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -23,17 +23,20 @@ def mars_news(browser):
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
+     # Add try/except for error handling
+    try:
     slide_elem = news_soup.select_one('div.list_text')
     slide_elem.find('div', class_='content_title')
-
 
     # Use the parent element to find the first `a` tag and save it as `news_title`
     news_title = slide_elem.find('div', class_='content_title').get_text()
   
-
     # Use the parent element to find the paragraph text
     news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
     
+    except AttributeError:
+        return None, None
+        
     return news_title, news_p
 
 # Visit URL
