8082c99ec4947dbb7345439a90836addeb5b2975
monsecc01 <81447450+monsecc01@users.noreply.github.com>
2021-05-30 12:45:33 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -4,12 +4,26 @@
 # # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
     # Visit the mars nasa news site
@@ -83,4 +97,7 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-browser.quit()
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
